a5dd3879750704f1126f12189531c83a79d36476
Sheng <shengxi05@gmail.com>
2025-07-14 13:31:06 +0000

    Added car_in_field hash map to track all car positions

--- src/simulation.py
+++ src/simulation.py
@@ -13,6 +13,7 @@ class Simulation:
         self.field = Field(width=field_size[0], height=field_size[1])
         self.cars = {}
         self.car_names = set()
+        self.cars_in_field = {}
 
     def add_car(self, car):
         """Add a car to the simulation."""
@@ -22,6 +23,9 @@ class Simulation:
         if car.name in self.car_names:
             raise ValueError("Car with this name already exists.")
         
+        if car.position in self.cars_in_field:
+            raise ValueError("Position already occupied by another car.")
+        
         #check if the car's position is within the field bounds
         if not (0 <= car.position[0] < self.field.width and 0 <= car.position[1] < self.field.height):
             raise ValueError("Position out of bounds.")
@@ -29,6 +33,7 @@ class Simulation:
         # Get index for car based on add logic (e.g., next available index)
         car_index = len(self.cars)
         self.cars[car_index] = car
+        self.cars_in_field[car.position] = car
         self.car_names.add(car.name)
 
         
