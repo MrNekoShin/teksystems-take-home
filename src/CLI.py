37f8c49b35195cc14b93e3ad5b65691e39479095
Sheng <shengxi05@gmail.com>
2025-07-15 14:12:32 +0000

    Add welcome method to CLI for displaying a welcome message

--- src/CLI.py
+++ src/CLI.py
@@ -3,4 +3,6 @@ class CLI:
     def __init__(self):
         """Initialize the CLI."""
 
-    
+    def welcome(self):
+        """Display the welcome message."""
+        print("Welcome to Auto Driving Car Simulation!\n")
