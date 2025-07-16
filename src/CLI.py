90e34f4ff208023595cc2170c611336f64cf1c8e
Sheng <shengxi05@gmail.com>
2025-07-16 07:04:55 +0000

    Add CLI messages tests and CLI messages

--- src/CLI.py
+++ src/CLI.py
@@ -2,7 +2,42 @@
 class CLI:
     def __init__(self):
         """Initialize the CLI."""
+        self.simulation = None  # This will hold the simulation instance once created
 
     def welcome(self):
         """Display the welcome message."""
         print("Welcome to Auto Driving Car Simulation!\n")
+
+    def create_field_message(self):
+        """Prompt user to create a field."""
+        print("Please enter the width and height of the simulation field in x y format:")
+
+    def field_created_message(self, width, height):
+        """Display confirmation of field creation."""
+        print(f"You have created a field of {width} x {height}.")
+
+    def options_menu_message(self):
+        """Display the options menu."""
+        print("Please choose from the following options:")
+        print("[1] Add a car to field")
+        print("[2] Run simulation")
+
+    def add_car_name_message(self):
+        """Prompt user to enter car name."""
+        print("Please enter the name of the car:")
+
+    def add_car_initial_position_and_orientation_message(self, car_name):
+        """Prompt user to enter car position."""
+        print(f"Please enter initial position of car {car_name} in x y Direction format:")
+
+    def add_car_instructions_message(self, car_name):
+        """Prompt user to enter car instructions."""
+        print(f"Please enter the commands for car {car_name}:")
+
+    def list_cars_message(self):
+        """Display the list of cars in the simulation."""
+        print("List of cars in the simulation:")
+
+        if self.simulation and self.simulation.cars:
+            for car in self.simulation.cars.values():
+                print(car)
--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -1,7 +1,7 @@
 import pytest
 from src.CLI import CLI
 
-class TestCLI:
+class TestCLIMessages:
     """Test cases for the CLI class."""
 
     @pytest.fixture(autouse=True)
@@ -19,3 +19,91 @@ class TestCLI:
 
         captured = capsys.readouterr()
         assert "Welcome to Auto Driving Car Simulation!\n" in captured.out, "Welcome message should be displayed."
+
+    def test_cli_create_field_message(self, capsys):
+        """Test the CLI create field method."""
+
+        self.cli.create_field_message()
+
+        captured = capsys.readouterr()
+        assert "Please enter the width and height of the simulation field in x y format:" in captured.out, "Field creation message should be displayed."
+
+    def test_cli_field_created_message(self, capsys):
+        """Test the CLI field created message."""
+        width, height = 10, 10
+        self.cli.field_created_message(width, height)
+
+        captured = capsys.readouterr()
+        assert f"You have created a field of {width} x {height}." in captured.out, "Field creation confirmation should be displayed."
+
+    def test_cli_options_menu_message(self, capsys):
+        """Test the CLI options menu message."""
+        self.cli.options_menu_message()
+        
+        captured = capsys.readouterr()
+        assert "Please choose from the following options:" in captured.out, "Options menu message should be displayed."
+        assert "[1] Add a car to field" in captured.out, "Option to add a car should be displayed."
+        assert "[2] Run simulation" in captured.out, "Option to run simulation should be displayed."
+
+    def test_cli_add_car_name_message(self, capsys):
+        """Test the CLI add car message."""
+        self.cli.add_car_name_message()
+        
+        captured = capsys.readouterr()
+        assert "Please enter the name of the car:" in captured.out, "Car name prompt should be displayed."
+
+    def test_cli_add_car_initial_position_and_orientation_message(self, capsys):
+        """Test the CLI add car position message."""
+
+        car_name = "TestCar"
+
+        self.cli.add_car_initial_position_and_orientation_message(car_name)
+        
+        captured = capsys.readouterr()
+        assert f"Please enter initial position of car {car_name} in x y Direction format:" in captured.out, "Car position prompt should be displayed."
+
+    def test_cli_add_car_instructions_message(self, capsys):
+        """Test the CLI add car instructions message."""
+        car_name = "TestCar"
+
+        self.cli.add_car_instructions_message(car_name)
+        
+        captured = capsys.readouterr()
+        assert f"Please enter the commands for car {car_name}:" in captured.out, "Car instructions prompt should be displayed."
+
+    def test_cli_list_no_cars_message(self, capsys):
+        """Test the CLI list cars message."""
+        self.cli.list_cars_message()
+        
+        captured = capsys.readouterr()
+        assert "List of cars in the simulation:" in captured.out, "List cars message should be displayed."
+
+
+    def test_cli_list_cars_with_details(self, capsys):
+        """Test the CLI list cars with details."""
+        from src.simulation import Simulation
+        from src.car import Car
+    
+        self.cli.simulation = Simulation(field_size=(10, 10))
+        self.cli.simulation.add_car(Car(name="B", position=(1, 2), orientation='N', instructions="FFRFFFFRRL"))
+
+        self.cli.list_cars_message()
+        
+        captured = capsys.readouterr()
+
+        assert "B, (1, 2) N, FFRFFFFRRL" in captured.out, "Car details should be displayed."
+
+    def test_cli_list_multiple_cars(self, capsys):
+        """Test the CLI list multiple cars."""
+        from src.simulation import Simulation
+        from src.car import Car
+    
+        self.cli.simulation = Simulation(field_size=(10, 10))
+        self.cli.simulation.add_car(Car(name="Car1", position=(0, 0), orientation='N', instructions="F"))
+        self.cli.simulation.add_car(Car(name="Car2", position=(1, 1), orientation='W', instructions="L"))
+        
+        self.cli.list_cars_message()
+
+        captured = capsys.readouterr()
+        assert "Car1, (0, 0) N, F" in captured.out, "First car details should be displayed."
+        assert "Car2, (1, 1) W, L" in captured.out, "Second car details should be displayed."
