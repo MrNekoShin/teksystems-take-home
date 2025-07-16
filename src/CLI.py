875790a55d07cff52aae2ebbaeb8218f06014bdd
Sheng <shengxi05@gmail.com>
2025-07-16 13:20:07 +0000

    Add keyboard interrupt handling to user input methods in CLI

--- src/CLI.py
+++ src/CLI.py
@@ -82,108 +82,132 @@ class CLI:
             return width, height
         except ValueError as e:
             raise ValueError(e)
+        except KeyboardInterrupt:
+            raise
     
     def get_options_menu_input(self):
         """Get the user's choice from the options menu."""
-        choice = input()
+        try:
+            choice = input()
 
-        if choice not in ['1', '2']:
-            raise ValueError("Invalid choice. Please enter 1 or 2.")
-        
-        return choice
+            if choice not in ['1', '2']:
+                raise ValueError("Invalid choice. Please enter 1 or 2.")
+            
+            return choice
+        except KeyboardInterrupt:
+            raise
     
     def get_car_name_input(self):
         """Get the car name from user input."""
-        car_name = input().strip()
-        
-        if not car_name:
-            raise ValueError("Car name cannot be empty.")
-        
-        return car_name
+        try:
+            car_name = input().strip()
+            
+            if not car_name:
+                raise ValueError("Car name cannot be empty.")
+            
+            return car_name
+        except KeyboardInterrupt:
+            raise
     
     def get_car_initial_position_and_orientation_input(self):
         """Get the car's initial position and orientation."""
-        position = input().strip()
-
-        pattern = r'^\d+\s+\d+\s+[NESW]$'
-        if not re.match(pattern, position):
-            raise ValueError("Invalid input. Please enter in x y Direction format where Direction is one of N, E, S, W.")
-
         try:
-            x, y, direction = position.split()
-            x, y = int(x), int(y)
-            
-            return (x, y), direction
-        except ValueError as e:
-            raise ValueError(e)
+            position = input().strip()
+
+            pattern = r'^\d+\s+\d+\s+[NESW]$'
+            if not re.match(pattern, position):
+                raise ValueError("Invalid input. Please enter in x y Direction format where Direction is one of N, E, S, W.")
+
+            try:
+                x, y, direction = position.split()
+                x, y = int(x), int(y)
+                
+                return (x, y), direction
+            except ValueError as e:
+                raise ValueError(e)
+        except KeyboardInterrupt:
+            raise
         
     def get_car_instructions_input(self):
         """Get the car's instructions."""
-        instructions = input().strip()
-        
-        # Validate that instructions only contain valid characters (e.g., L, R, F)
-        if not all(char in 'LRF' for char in instructions):
-            raise ValueError("Invalid command. Only 'L', 'R', and 'F' are allowed.")
-        
-        return instructions
+        try:
+            instructions = input().strip()
+            
+            # Validate that instructions only contain valid characters (e.g., L, R, F)
+            if not all(char in 'LRF' for char in instructions):
+                raise ValueError("Invalid command. Only 'L', 'R', and 'F' are allowed.")
+            
+            return instructions
+        except KeyboardInterrupt:
+            raise
     
     def get_after_simulation_options_menu_input(self):
         """Get the user's choice after simulation."""
-        choice = input()
+        try:
+            choice = input()
 
-        if choice not in ['1', '2']:
-            raise ValueError("Invalid choice. Please enter 1 or 2.")
-        
-        return choice
+            if choice not in ['1', '2']:
+                raise ValueError("Invalid choice. Please enter 1 or 2.")
+            
+            return choice
+        except KeyboardInterrupt:
+            raise
     
     ### Flow control methods ###
 
     def add_car_loop(self):
         """Loop to add cars to the simulation."""
-        while True:
-            try:
-                self.options_menu_message()
-                choice = self.get_options_menu_input()
-            except ValueError as e:
-                print(e)
-                continue
+        try:
+            while True:
+                try:
+                    self.options_menu_message()
+                    choice = self.get_options_menu_input()
+                except ValueError as e:
+                    print(e)
+                    continue
 
-            if choice == '1':
-                self.add_car()
-            elif choice == '2':
-                break
+                if choice == '1':
+                    self.add_car()
+                elif choice == '2':
+                    break
+        except KeyboardInterrupt:
+            raise
 
     def add_car(self):
         """Add a car to the simulation."""
-        while True:
-            try:
-                self.add_car_name_message()
-                car_name = self.get_car_name_input()
-                break
+        try:
+            while True:
+                try:
+                    self.add_car_name_message()
+                    car_name = self.get_car_name_input()
+                    break
+                
+                except ValueError as e:
+                    print(e)
+                    continue
             
-            except ValueError as e:
-                print(e)
-                continue
-        
-        while True:
-            try:
-                self.add_car_initial_position_and_orientation_message(car_name)
-                position, orientation = self.get_car_initial_position_and_orientation_input()
-                break
+            while True:
+                try:
+                    self.add_car_initial_position_and_orientation_message(car_name)
+                    position, orientation = self.get_car_initial_position_and_orientation_input()
+                    break
+                
+                except ValueError as e:
+                    print(e)
+                    continue
             
-            except ValueError as e:
-                print(e)
-                continue
-        
-        while True:
-            try:
-                self.add_car_instructions_message(car_name)
-                instructions = self.get_car_instructions_input()
-                break
-            
-            except ValueError as e:
-                print(e)
-                continue
+            while True:
+                try:
+                    self.add_car_instructions_message(car_name)
+                    instructions = self.get_car_instructions_input()
+                    break
+                
+                except ValueError as e:
+                    print(e)
+                    continue
+
+        except KeyboardInterrupt:
+            raise
         
         # Create and add the car to the simulation
         car = Car(name=car_name, position=position, orientation=orientation, instructions=instructions)
@@ -191,29 +215,35 @@ class CLI:
     
     def create_field_loop(self):
         """Loop to create the simulation field."""
-        while True:
-            try:
-                self.create_field_message()
-                width, height = self.get_field_input()
-                break
-            
-            except ValueError as e:
-                print(e)
-                continue
+        try:
+            while True:
+                try:
+                    self.create_field_message()
+                    width, height = self.get_field_input()
+                    break
+                
+                except ValueError as e:
+                    print(e)
+                    continue
+        except KeyboardInterrupt:
+            raise
             
         self.simulation = Simulation(field_size=(width, height))
         self.field_created_message(width, height)
 
     def after_simulation_loop(self):
         """Loop to handle options after simulation."""
-        while True:
-            try:
-                self.after_simulation_options_menu_message()
-                choice = self.get_after_simulation_options_menu_input()
-                return choice
-            except ValueError as e:
-                print(e)
-                continue
+        try:
+            while True:
+                try:
+                    self.after_simulation_options_menu_message()
+                    choice = self.get_after_simulation_options_menu_input()
+                    return choice
+                except ValueError as e:
+                    print(e)
+                    continue
+        except KeyboardInterrupt:
+            raise
         
 
 
@@ -221,17 +251,20 @@ class CLI:
         """Main loop to run the CLI."""
         self.welcome()
         
-        while True:
-            self.create_field_loop()
-            self.add_car_loop()
+        try:
+            while True:
+                self.create_field_loop()
+                self.add_car_loop()
+                
+                self.list_cars_message()
+                self.simulation.run_simulation()
+                self.simulation_results_message()
+
+                choice = self.after_simulation_loop()
+
+                if choice == '2':
+                    break
             
-            self.list_cars_message()
-            self.simulation.run_simulation()
-            self.simulation_results_message()
-
-            choice = self.after_simulation_loop()
-
-            if choice == '2':
-                break
-        
-        self.goodbye()
+            self.goodbye()
+        except KeyboardInterrupt:
+            print("\nSimulation interrupted. Goodbye!")
